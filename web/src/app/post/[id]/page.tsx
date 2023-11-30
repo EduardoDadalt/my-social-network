c32a81e40fcff9e4515f9abdf5a2df53ec6360a1
Eduardo Dadalt <eduardodadalt1@gmail.com>
2023-11-29 23:58:51 -0300

    Fix somethings in PostPage

--- web/src/app/post/[id]/page.tsx
+++ web/src/app/post/[id]/page.tsx
@@ -8,20 +8,25 @@ export default async function PostPage({
   const post = await PostController.getPost(id);
   if (post === null) return <div>404</div>;
   return (
-    <main className="flex items-center justify-center">
-      <div className="border shadow-xl rounded-xl min-w-[50%] p-2">
-        <div className="flex flex-row">
+    <main className="flex bg-gray-950 min-h-screen flex items-start p-4 pt-12 md:px-12 lg:px-24">
+      <div className="border shadow-xl rounded-xl w-full p-2 bg-slate-100 max-w-2xl">
+        <div className="flex flex-row space-x-2">
           <Image
             src={`/image/${post.author.avatarId}`}
             alt={`Avatar of ${post.author.name}`}
-            height={10}
-            width={10}
+            height={20}
+            width={20}
+            className="rounded-full aspect-square object-cover"
           />
-          <div className="text-sm"> {post.author.name}</div>
+          <div className="text-sm">{post.author.name}</div>
         </div>
         <h1 className="text-lg font-bold">{post.title}</h1>
-        <p>{post.content}</p>
-        <div></div>
+        <div className="space-y-1">
+          {post.content.split("\\n").map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+        <div>{post._count.likes} likes</div>
       </div>
     </main>
   );
