4192cb7cdb372f2561736b7b59c5581edf84576c
Eduardo Dadalt <eduardodadalt1@gmail.com>
2023-12-01 00:20:42 -0300

    Starting to make a image post endpoint

---
+++ web/src/app/image/[...id]/route.ts
@@ -0,0 +1,36 @@
+import { ImageController } from "@/controller/ImageController";
+import { NextRequest, NextResponse } from "next/server";
+import sharp from "sharp";
+
+export async function GET(
+  request: NextRequest,
+  {
+    params: {
+      id: [id],
+    },
+  }: { params: { id: string[] } }
+) {
+  const buffer = await ImageController.getBinaryImage(id);
+  if (!buffer) {
+    return NextResponse.json(
+      {
+        message: "Image not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+  const response = new NextResponse(buffer);
+  response.headers.set("Content-Type", "image/webp");
+  return response;
+}
+
+export async function POST(request: NextRequest) {
+  const blob = await request.blob();
+  const buffer = await blob.arrayBuffer();
+  const bufferOutput = await sharp(buffer).webp({ quality: 80 }).toBuffer();
+  return new NextResponse(bufferOutput, {
+    headers: { "Content-Type": "image/webp" },
+  });
+}
